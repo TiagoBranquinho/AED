e78836b9722cd5cc51f9b05186e174b5274e1f7f
Alexandre Correia <correiaalexandrealex@gmail.com>
2021-12-02 21:50:33 +0000

    Added Service class

---
+++ Service.cpp
@@ -0,0 +1,5 @@
+//
+// Created by alexandre on 02/12/21.
+//
+
+#include "Service.h"
---
+++ Service.h
@@ -0,0 +1,14 @@
+//
+// Created by alexandre on 02/12/21.
+//
+
+#ifndef PROJETOAEROPORTO_SERVICE_H
+#define PROJETOAEROPORTO_SERVICE_H
+
+
+class Service {
+
+};
+
+
+#endif //PROJETOAEROPORTO_SERVICE_H
