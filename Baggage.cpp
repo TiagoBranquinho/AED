e8f1610612259bacd11a17cbeabac88a051d2d7f
Tiago Branquinho <tiagobranquinho@MacBook-Air-de-Tiago.local>
2021-12-03 11:09:00 +0000

    Update Baggage Class

--- Baggage.cpp
+++ Baggage.cpp
@@ -3,3 +3,44 @@
 //
 
 #include "Baggage.h"
+
+Baggage::Baggage() {
+    weight = 0;
+    special = false;
+}
+
+Baggage::Baggage(double weight, bool special) {
+    this->weight = weight;
+    this->special = special;
+    checkWeight();
+}
+
+double Baggage::getWeight() {
+    return weight;
+}
+
+void Baggage::setWeight(double weight) {
+    this->weight = weight;
+    checkWeight();
+
+}
+
+bool Baggage::isSpecial() {
+    return special;
+}
+
+void Baggage::setSpecial(bool special) {
+    this->special = special;
+
+}
+
+bool Baggage::isOverweight() {
+    return(weight <= maxWeight ? false:true);
+}
+
+void Baggage::checkWeight() {
+    if(weight <= maxWeight)
+        overweight = false;
+    else
+        overweight = true;
+}
--- Baggage.h
+++ Baggage.h
@@ -4,10 +4,24 @@
 
 #ifndef PROJETOAEROPORTO_BAGGAGE_H
 #define PROJETOAEROPORTO_BAGGAGE_H
+#include <string>
 
+using namespace std;
 
 class Baggage {
-
+private:
+    double weight, maxWeight = 25;
+    bool special;
+    bool overweight;
+public:
+    Baggage();
+    Baggage(double weight, bool special);
+    double getWeight();
+    void setWeight(double weight);
+    bool isSpecial();
+    void setSpecial(bool special);
+    bool isOverweight();
+    void checkWeight();
 };
 
 
