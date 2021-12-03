681b3b42bf9d7386a4197ed63179abeaf94b8c8d
unknown <henri11silvafcc@gmail.com>
2021-12-03 22:08:08 +0000

    Flight class modified

--- Flight.cpp
+++ Flight.cpp
@@ -3,3 +3,53 @@
 //
 
 #include "Flight.h"
+
+Flight::Flight() = default;
+
+Flight::Flight(unsigned int number, Date date, unsigned int duration, string origin, string destiny) {
+    this->number = number;
+    this->date = date;
+    this->duration = duration;
+    this->origin = origin;
+    this->destiny = destiny;
+}
+
+void Flight::setNumber(unsigned int number) {
+    this->number = number;
+}
+
+unsigned int Flight::getNumber() {
+    return number;
+}
+
+void Flight::setDate(Date date) {
+    this->date = date;
+}
+
+Date Flight::getDate() {
+    return date;
+}
+
+void Flight::setDuration(unsigned int duration) {
+    this->duration = duration;
+}
+
+unsigned int Flight::getDuration() {
+    return duration;
+}
+
+void Flight::setOrigin(string origin) {
+    this->origin = origin;
+}
+
+string Flight::getOrigin() {
+    return origin;
+}
+
+void Flight::setDestiny(string destiny) {
+    this->destiny = destiny;
+}
+
+string Flight::getDestiny() {
+    return destiny;
+}
--- Flight.h
+++ Flight.h
@@ -6,7 +6,33 @@
 #define PROJETOAEROPORTO_FLIGHT_H
 
 
+#include "time.h"
+#include "Date.h"
+
+using namespace std;
+
+
 class Flight {
+
+private:
+    unsigned int number;
+    Date date;
+    unsigned int duration; //in seconds
+    string origin, destiny;
+
+public:
+    Flight();
+    Flight(unsigned int number, Date date, unsigned int duration, string origin, string destiny);
+    void setNumber(unsigned int number);
+    unsigned int getNumber();
+    void setDate(Date date);
+    Date getDate();
+    void setDuration(unsigned int duration);
+    unsigned int getDuration();
+    void setOrigin(string origin);
+    string getOrigin();
+    void setDestiny(string destiny);
+    string getDestiny();
 
 };
 
