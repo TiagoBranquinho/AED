70de85bfeac4563e7bfb7d2a26938267a1d5210a
unknown <henri11silvafcc@gmail.com>
2021-12-04 14:42:37 +0000

    Constructed groundTransportation class

--- groundTransportation.cpp
+++ groundTransportation.cpp
@@ -3,3 +3,29 @@
 //
 
 #include "groundTransportation.h"
+
+groundTransportation::groundTransportation() = default;
+
+groundTransportation::groundTransportation(string type, unsigned int distance) {
+    this->type = type;
+    this->distance = distance;
+
+}
+
+void groundTransportation::setType(string type) {
+    this->type = type;
+}
+
+void groundTransportation::setDistance(unsigned int distance) {
+    this->distance = distance;
+}
+
+string groundTransportation::getType() {
+    return type;
+}
+
+unsigned int groundTransportation::getDistance() {
+    return distance;
+}
+
+
--- groundTransportation.h
+++ groundTransportation.h
@@ -5,8 +5,27 @@
 #ifndef PROJETOAEROPORTO_GROUNDTRANSPORTATION_H
 #define PROJETOAEROPORTO_GROUNDTRANSPORTATION_H
 
+#include <string>
+
+using namespace std;
+
 
 class groundTransportation {
+
+private:
+    string type;
+    unsigned int distance;
+    //Schedule schedule; Class schedule should be implemented?
+
+public:
+    groundTransportation();
+    groundTransportation(string type, unsigned int distance);
+    void setType(string type);
+    void setDistance(unsigned int distance);
+    string getType();
+    unsigned int getDistance();
+
+
 
 };
 
