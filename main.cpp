56c2b74efc0c639d063d7f7373c3267cb34df499
Tiago Branquinho <tiagobranquinho@Air-de-Tiago.lan>
2021-12-16 20:01:24 +0000

    Some modifications

--- main.cpp
+++ main.cpp
@@ -4,14 +4,16 @@
 
 #include "include/AppStarter.h"
 
-/*int main() {
-    AppStarter app;
-    app.start();
+int main() {
+    //AppStarter app;
+    App app;
+    //app.start();
+    app.writePlanesFile();
     return 0;
-}*/
-
+}
+/*
 int main(int argc, char* argv[]) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
-
+*/
