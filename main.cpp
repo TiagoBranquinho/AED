66df6c8e90d6a1c7cec35b9fcebb02fb8e545c53
Alexandre Correia <correiaalexandrealex@gmail.com>
2021-12-16 10:52:00 +0000

    Uncomment main test funtion

--- main.cpp
+++ main.cpp
@@ -4,14 +4,13 @@
 
 #include "include/App.h"
 
-int main() {
+/*int main() {
     App application = App();
     application.start();
     return 0;
-}
+}*/
 
-/*
 int main(int argc, char* argv[]) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
-}*/
+}
